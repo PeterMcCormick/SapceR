cc164895c6d4c2a7b35636083f9566aeefa24783
Anthony Jordan <Kain235@gmail.com>
2018-04-02 15:13:35 -0400

    help

--- spec/rest-serviceSpec.ts
+++ spec/rest-serviceSpec.ts
@@ -1,10 +1,11 @@
-import {RestServiceProvider} from "../providers/rest-service/rest-service";
+import {RestServiceProvider} from "../../providers/rest-service/rest-service";
 import {HttpClient} from "@angular/common/http";
 
 describe("Rest service test", () =>{
   let sut = new RestServiceProvider("test");
+  spyOn(sut,"get");
   it('should ', () => {
     sut.get()
-
+    expect(sut).toHaveBeenCalled();
   });
 });
