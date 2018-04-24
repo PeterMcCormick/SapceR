e20bf83fcf4fb967341484b68a37eadbc93e97bd
Anthony Jordan <Kain235@gmail.com>
2018-04-24 16:36:52 -0400

    Added storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, ViewChild} from '@angular/core';
 import {Nav, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {Storage} from "@ionic/storage";
 
 import { HomePage } from '../pages/home/home';
 import { LaunchListPage} from "../pages/launch-list/launch-list";
@@ -16,7 +17,7 @@ export class MyApp {
   rootPage:any = HomePage;
   pages: Array<{title: string, component: any}>;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private storage: Storage) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -29,7 +30,18 @@ export class MyApp {
         {title: "ISS Location", component: IssPage},
         {title: "People in Space", component: PeoplePage},
       ];
+
     });
+    var people = [
+      {name: "Anton Shkaplerov", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Anton_Shkaplerov"},
+      {name: "Scott Tingle", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Scott_D._Tingle"},
+      {name: "Norishige Kanai", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Norishige_Kanai"},
+      {name: "Oleg Artemyev", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Oleg_Artemyev"},
+      {name: "Andrew Feustel", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Andrew_J._Feustel"},
+      {name: "Richard Arnold", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Richard_R._Arnold"}
+    ];
+
+    this.storage.set('people', people);
   }
 
   openPage(page){
--- src/pages/people/people.ts
+++ src/pages/people/people.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage} from "@ionic/storage";
 import { InAppBrowser } from "@ionic-native/in-app-browser";
 
 /**
@@ -18,26 +19,31 @@ import { InAppBrowser } from "@ionic-native/in-app-browser";
 export class PeoplePage {
   numOfPeople: number;
   people: Array<{name: string, spaceCraft: string, wikiPage: string}>;
+  peopleFromStorage: Array<{name: string, spaceCraft: string, wikiPage: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private inAppBrowser: InAppBrowser) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private inAppBrowser: InAppBrowser, private storage: Storage) {
 
-    this.people = [
-      {name: "Anton Shkaplerov", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Anton_Shkaplerov"},
-      {name: "Scott Tingle", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Scott_D._Tingle"},
-      {name: "Norishige Kanai", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Norishige_Kanai"},
-      {name: "Oleg Artemyev", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Oleg_Artemyev"},
-      {name: "Andrew Feustel", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Andrew_J._Feustel"},
-      {name: "Richard Arnold", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Richard_R._Arnold"}
-    ];
-    this.numOfPeople = this.people.length;
-  }
+    // this.people = [
+    //   {name: "Anton Shkaplerov", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Anton_Shkaplerov"},
+    //   {name: "Scott Tingle", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Scott_D._Tingle"},
+    //   {name: "Norishige Kanai", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Norishige_Kanai"},
+    //   {name: "Oleg Artemyev", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Oleg_Artemyev"},
+    //   {name: "Andrew Feustel", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Andrew_J._Feustel"},
+    //   {name: "Richard Arnold", spaceCraft: "ISS", wikiPage: "https://en.wikipedia.org/wiki/Richard_R._Arnold"}
+    // ];
+    // this.numOfPeople = this.people.length;
+    // this.storage.set('people', this.people);
+    this.getFromStorage('people');
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PeoplePage');
+
   }
 
   openWikiPage(page){
     this.inAppBrowser.create(page);
+
+  }
+  async getFromStorage(key: string):Array{
+    await this.storage.get(key).then((data) => this.peopleFromStorage = data);
 
   }
 
